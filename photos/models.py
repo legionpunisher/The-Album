2b7c236fbc936717567577cb1acb1307b8e8a2c8
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 18:54:42 +0300

    created an uploader class for the person uploading the images and removed the initial image class

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField()
-    image_name = models.CharField(max_length=30)
-    Description = models.CharField(max_length=30)
+class Uploader(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+   
+   
     
+    def __str__(self):
+        return self.first_name
