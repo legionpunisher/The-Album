0e5f67dbd511384f800874276dc40ff0ec6e2190
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 19:35:11 +0300

    added a category class for the pic category

--- photos/models.py
+++ photos/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class Uploader(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-   
-   
-    
+    email = models.EmailField() 
+       
     def __str__(self):
         return self.first_name
+class category(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+   
