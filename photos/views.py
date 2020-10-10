957a7766d64c67793c0bb864a3564f17c6c1738b
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 13:54:14 +0300

    modified our welcome message

--- photos/views.py
+++ photos/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to my Galleria')
+    return HttpResponse('Welcome to the World of Photo Beauty')
