2148f2589349a232ba5592d899142375361d7e41
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 22:10:15 +0300

    created a testing instance

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 from .models import Uploader,Image,Category,Location
 # Create your tests here.
+class UploaderTestClass(TestCase):
+    def setUp(self):
+        self.john=Uploader(first_name='john',last_name='lee',email='123@gmail.com')
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.john,Uploader))
