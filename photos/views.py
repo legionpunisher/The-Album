0466cb5161cdb7d93f59c0e34ce96f5000fe4e7b
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 14:14:25 +0300

    created a welcome html template to hold our view welcome function

--- photos/views.py
+++ photos/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the World of Photo Beauty')
+    return render(request, 'welcome.html')
