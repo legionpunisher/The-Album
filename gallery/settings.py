0caafd46a187f9afed32c35d2f071dabd1ce533c
legionpunisher <mbuguamoses656@gmail.com>
2020-10-10 18:12:56 +0300

    added a database to our application

--- gallery/settings.py
+++ gallery/settings.py
@@ -63,14 +63,14 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'falcon',
+    'PASSWORD':'shadow',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
